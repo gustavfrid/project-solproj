23e288a3cf08b290476a01a7d8d572a786c2fb01
Gustav Frid <gustav.frid@gmail.com>
2022-01-29 08:10:03 +0100

    fetaure: test mui on auth component

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -3,6 +3,14 @@ import { useDispatch, useSelector, batch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import InputLabel from '@mui/material/InputLabel'
+import InputAdornment from '@mui/material/InputAdornment'
+import FormControl from '@mui/material/FormControl'
+import Visibility from '@mui/icons-material/Visibility'
+
 import { user } from '../reducers/user'
 import { API_URL } from '../utils/constants'
 
@@ -105,8 +113,38 @@ export const Auth = () => {
           }}
         />
         <button type='submit'>{formState}</button>
-        <p onClick={onChangeFormState}>{formState === 'signin' ? 'Create an account? Signup!' : 'Already user? Sign in!'}</p>
+        <p onClick={onChangeFormState}>
+          {formState === 'signin' ? 'Create an account? Signup!' : 'Already user? Sign in!'}
+        </p>
       </FormContainer>
+      <TextField
+        error
+        id='outlined-error-helper-text'
+        label='Error'
+        defaultValue='Hello World'
+        helperText='Incorrect entry.'
+      />{' '}
+      <FormControl sx={{ m: 1, width: '25ch' }} variant='outlined'>
+        <InputLabel htmlFor='outlined-adornment-password'>Password</InputLabel>
+        <OutlinedInput
+          id='outlined-adornment-password'
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          onChange={handleChange('password')}
+          endAdornment={
+            <InputAdornment position='end'>
+              <IconButton
+                aria-label='toggle password visibility'
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge='end'>
+                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label='Password'
+        />
+      </FormControl>
     </Container>
   )
 }
