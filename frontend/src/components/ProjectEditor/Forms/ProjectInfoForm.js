cb46d3486600be9bd36b5198a689c2c9506d1c98
Gustav Frid <gustav.frid@gmail.com>
2022-02-07 10:54:28 +0100

    cleanup

--- frontend/src/components/ProjectEditor/Forms/ProjectInfoForm.js
+++ frontend/src/components/ProjectEditor/Forms/ProjectInfoForm.js
@@ -1,6 +1,6 @@
 import { Stack } from '@chakra-ui/react'
 import { InputField } from '../FormFields/InputField'
-import { MapMapbox } from '../../Location/MapMapbox'
+import { MapboxSearch } from '../../Location/MapboxSearch'
 
 export const ProjectInfoForm = (props) => {
   const {
@@ -9,7 +9,7 @@ export const ProjectInfoForm = (props) => {
 
   return (
     <Stack spacing='20px'>
-      <MapMapbox />
+      <MapboxSearch />
       <InputField name={projectName.name} label={projectName.label} />
     </Stack>
   )
