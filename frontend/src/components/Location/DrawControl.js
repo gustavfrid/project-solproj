d43f20014f1f05093d7d4e75c17e3190ad498ce1
Gustav Frid <49785603+gustavfrid@users.noreply.github.com>
2024-06-17 13:49:28 +0200

    chore: remove comments

--- frontend/src/components/Location/DrawControl.js
+++ frontend/src/components/Location/DrawControl.js
@@ -2,25 +2,12 @@ import MapboxDraw from '@mapbox/mapbox-gl-draw'
 import { useControl } from 'react-map-gl'
 
 export const DrawControl = (props) => {
-  console.log('🚀 ~ file: DrawControl.js ~ line 5 ~ DrawControl ~ props', props)
 
   useControl(
     ({ map }) => {
       map.on('draw.create', props.onCreate)
       map.on('draw.update', props.onUpdate)
       map.on('draw.delete', props.onDelete)
-      // const draw = new MapboxDraw(props)
-
-      // if (props.mapFeatures) {
-      //   console.log(
-      //     '🚀 ~ file: DrawControl.js ~ line 14 ~ DrawControl ~ props.mapFeatures',
-      //     JSON.stringify({ features: props.mapFeatures })
-      //   )
-      //   const feature = { type: 'Point', coordinates: [18.070742255316343, 59.32496507200476] }
-      //   // draw.add(JSON.stringify(props.mapFeatures[0]))
-      //   const featureIds = draw.add(feature)
-      //   console.log('🚀 ~ file: DrawControl.js ~ line 20 ~ DrawControl ~ featureIds', featureIds)
-      // }
       return new MapboxDraw(props)
     },
     ({ map }) => {
